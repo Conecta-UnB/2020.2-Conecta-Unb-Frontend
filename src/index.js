8b1aa04d79770f95bb2dc959d426bc5c5f187328
Rhuan Marques <magicknight@hotmail.com.br>
2021-03-20 17:39:55 -0300

    criação da branch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 // eslint-disable-next-line import/no-named-as-default
 import reportWebVitals from './reportWebVitals';
