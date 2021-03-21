bb527f78b8772e16470c054e2f92b6121f34f4b2
Rhuan Marques <magicknight@hotmail.com.br>
2021-03-21 13:16:32 -0300

    organização das pastas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './global.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      oi
+      <Home />
     </div>
   );
 }
